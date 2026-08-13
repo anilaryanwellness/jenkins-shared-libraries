0245919ee101ea994b151a59b0ebfdccbd96e693
anilaryanwellness <anilaryanwellness@gmail.com>
2026-08-14 00:11:57 +0530

    Update update_k8s_manifests.groovy

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -7,8 +7,8 @@ def call(Map config = [:]) {
     def imageTag = config.imageTag ?: error("Image tag is required")
     def manifestsPath = config.manifestsPath ?: 'kubernetes'
     def gitCredentials = config.gitCredentials ?: 'git'
-    def gitUserName = config.gitUserName ?: 'Jenkins CI'
-    def gitUserEmail = config.gitUserEmail ?: 'jenkins@example.com'
+    def gitUserName = config.gitUserName ?: 'anilaryanwellness'
+    def gitUserEmail = config.gitUserEmail ?: 'anilaryanwellness@gmail.com'
     
     echo "Updating Kubernetes manifests with image tag: ${imageTag}"
     
